5f4042331f2114edce7397cdefb8464a3fd93fba
OldKinn <leonooo@163.com>
2016-07-27 11:47:22 +0800

    自动化构建工具完善,增加插件自动抽取功能

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,36 @@
 var gulp = require('gulp');
+var async = require('async');
 
-gulp.task('assets', function(callback) {
-
+gulp.task('assets', function (callback) {
+    var assets = [
+        {
+            module: 'bootstrap',
+            desc: '视图层使用的UI框架',
+            src: [
+                './bower_components/bootstrap/dist/css/bootstrap.min.css',
+                './bower_components/bootstrap/dist/fonts/**'
+            ],
+            base: './bower_components/bootstrap/dist',
+            dest: './bundler/public/assets/bootstrap'
+        },
+        {
+            module: 'requireJs',
+            desc: 'RequireJS',
+            src: './bower_components/requirejs/require.js',
+            dest: './bundler/public/assets/libs'
+        }
+    ];
+    async.each(assets, function (item, callback) {
+        console.time('extract ' + item.module);
+        var options = item.base ? {base: item.base} : {};
+        var stream = gulp.src(item.src, options);
+        stream.on('end', function () {
+            console.timeEnd('extract ' + item.module);
+            console.log('抽取 %s 已完成!', item.module);
+            callback();
+        });
+        stream.pipe(gulp.dest(item.dest));
+    }, function (error) {
+        callback(error);
+    });
 });
